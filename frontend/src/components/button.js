b433f4e6453b788439c95126450c1055af528f53
pzehnder <pascal.zehnder@bf.uzh.ch>
2020-09-19 12:01:30 +0200

    Add initial styling for button

--- frontend/src/components/button.js
+++ frontend/src/components/button.js
@@ -2,7 +2,16 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledButton = styled.button`
-  color: blue;
+  height: 2rem;
+  min-width: 10rem;
+
+  font-size: 1.2rem;
+  font-weight: bold;
+
+  color: white;
+  background-color: #de7447;
+  border: 2px solid #de7447;
+  border-radius: 4px;
 `;
 
 export const Button = ({ onClick, children }) => {
--- frontend/src/routes/create-room.js
+++ frontend/src/routes/create-room.js
@@ -1,6 +1,16 @@
 import React from "react";
 import { v1 as uuid } from "uuid";
+import styled from "styled-components";
 import { Button } from "../components";
+
+const OverviewContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const CreateRoom = (props) => {
   function create() {
@@ -8,5 +18,9 @@ export const CreateRoom = (props) => {
     props.history.push(`/room/${id}`);
   }
 
-  return <Button onClick={create}>Create room</Button>;
+  return (
+    <OverviewContainer>
+      <Button onClick={create}>Create room</Button>
+    </OverviewContainer>
+  );
 };
